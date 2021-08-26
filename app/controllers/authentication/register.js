979d1fcc0637f30ddb339547070a5ecc5ee3babc
ronardj01 <ronardj01@gmail.com>
2021-08-26 16:17:03 +0200

    created register.js and authentication.js files

---
+++ app/controllers/authentication/register.js
@@ -0,0 +1,17 @@
+const { pool } = require("../../services/poolService.js");
+
+const query = `insert into users ( user_email, user_name, user_password)
+ values (lower($1), lower($2), $3)) returning user_id, user_name`;
+
+const register = async (req, res) => {
+  const { email, userName, password } = req.body;
+  const values = [email, userName, password];
+  try {
+    const result = await pool.query(query, values);
+    return res.json(result.rows);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = register;
