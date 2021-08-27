ddc6f4b818bbab826d9d96e6ec9ca002c5b8f999
ronardj01 <ronardj01@gmail.com>
2021-08-27 10:22:25 +0200

    hashed passord before saving

--- app/controllers/authentication/register.js
+++ app/controllers/authentication/register.js
@@ -1,11 +1,16 @@
 const pool = require("../../utils/poolService.js");
+const bcrypt = require("bcrypt");
 
 const query = `insert into users (user_email, user_name, user_password)
  values (lower($1), lower($2), $3) returning user_id, user_name`;
 
 const register = async (req, res) => {
   const { email, userName, password } = req.body;
-  const values = [email, userName, password];
+
+  const salt = await bcrypt.genSalt(10); //hash password before saving it to database
+  const bcryptPassword = await bcrypt.hash(password, salt);
+
+  const values = [email, userName, bcryptPassword];
   try {
     const result = await pool.query(query, values);
     return res.status(201).json(result.rows);
